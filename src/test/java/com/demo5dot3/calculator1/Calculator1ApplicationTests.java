92f3297f2e94b19a515b0d962dda1ef130a22947
student <hectormrg0823@gmail.com>
2021-02-06 15:45:52 +0000

    logic and test add

---
+++ src/test/java/com/demo5dot3/calculator1/Calculator1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo5dot3.calculator1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
